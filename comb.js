d4f5a61d0df1a79fbc7a66241b9f71319f5fb33f
masonb03 <bevinsm355@gmail.com>
2025-02-06 16:51:12 -0500

    Add files via upload

---
+++ comb.js
@@ -0,0 +1,20 @@
+//Step 1: Write 3 lines of code as a const
+//Step 2: Give each const a different variable
+//Step 3: Create a equation that will end up with the correct number for each
+const first = 5 + 5;
+const second = 2 * 20;
+const last = 40 - 1;
+//Step 4: Creat 3 console logs so I can test the strings to make sure they all end up correct and will show
+console.log(first);
+console.log(second);
+console.log(last);
+
+//Step 5: Create a alert
+//Step 6: Type in the message that is required
+//Step 7: Insert literal templates including the string as the end
+//Step 8: Surroudn the text with back ticks so the literal templates work
+
+
+alert(`You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: ${first} - ${second} - ${last}`);
+
+//Step 9: run the code in both terminal and goLive to make sure it works
